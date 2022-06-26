e3b4ddafc374b20ab8caa30ede844d150c45323b
ratchet <barakiva1@gmail.com>
2022-06-26 11:56:37 +0300

    laptop

--- src/app/index.js
+++ src/app/index.js
@@ -4,6 +4,25 @@ import path from 'path';
 import url from 'url';
 import http from 'http';
 import {Server} from 'socket.io'
+
+import mongoose from 'mongoose';
+import session from 'express-session';
+import passport from 'passport';
+import crypto from 'crypto';
+import local from 'passport-local';
+import connectMongo from 'connect-mongo';
+
+import dotenv from 'dotenv';
+const config = dotenv.config();
+const mongoUrl = process.env.DB_STRING;
+const localStrategy = local.Strategy;
+
+const connection = mongoose.createConnection(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// const MongoStore = new connectMongo(session, process.env.DB_STRING);
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
